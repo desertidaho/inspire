e545fcef4ef3771afa730e15b6a069991b526bed
Brett Wilcox <wilcox.brett@gmail.com>
2019-02-23 16:29:42 -0700

    Clock CSS.

--- app/components/greeting/greetingController.js
+++ app/components/greeting/greetingController.js
@@ -3,8 +3,15 @@ import GreetingService from "./greetingService.js";
 let _greetingService = new GreetingService()
 
 function drawData() {
-  document.querySelector('#clock').innerHTML = _greetingService.Clock
+  let time = _greetingService.Clock
+  document.querySelector('#clock').innerHTML = time
   document.querySelector('#date').innerHTML = _greetingService.Date
+  if (+time[1] >= 2) {
+    document.querySelector('#change-greeting').innerHTML = 'afternoon'
+  }
+  if (+time[1] >= 7 || +time[0] == 2) {
+    document.querySelector('#change-greeting').innerHTML = 'evening'
+  }
 }
 
 
