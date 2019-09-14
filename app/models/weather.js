e75b0d445613dd5d51f01ce535481b02fc843f6e
Brett Wilcox <brett.wilcox@intimetec.com>
2019-09-13 18:52:33 -0600

    Update sunset.

--- app/models/weather.js
+++ app/models/weather.js
@@ -1,19 +1,19 @@
 export default class Weather {
-  constructor(data) {
-    this.city = data.name
-    this.description = data.weather[0].description
-    this.kelvin = data.main.temp
-    this.celcius = (this.kelvin - 273.15).toFixed(0)
-    this.farenheit = (this.celcius * (9 / 5) + 32).toFixed(0)
-    this.windDegrees = data.wind.deg
-    this.windSpeed = data.wind.speed
-    this.sunrise = '0' + (parseInt(new Date(data.sys.sunrise * 1e3).toISOString().slice(-13, -11)) - 6) + new Date(data.sys.sunrise * 1e3).toISOString().slice(-11, -8)
-    this.sunset = (Math.abs(parseInt(new Date(data.sys.sunset * 1e3).toISOString().slice(-13, -11)) - 12) + 12) + new Date(data.sys.sunset * 1e3).toISOString().slice(-11, -8)
-    this.icon = data.weather[0].icon
-  }
+    constructor(data) {
+        this.city = data.name
+        this.description = data.weather[0].description
+        this.kelvin = data.main.temp
+        this.celcius = (this.kelvin - 273.15).toFixed(0)
+        this.farenheit = (this.celcius * (9 / 5) + 32).toFixed(0)
+        this.windDegrees = data.wind.deg
+        this.windSpeed = data.wind.speed
+        this.sunrise = '0' + (parseInt(new Date(data.sys.sunrise * 1e3).toISOString().slice(-13, -11)) - 6) + new Date(data.sys.sunrise * 1e3).toISOString().slice(-11, -8)
+        this.sunset = (Math.abs(parseInt(new Date(data.sys.sunset * 1e3).toISOString().slice(-13, -11)) - 12) + 8) + new Date(data.sys.sunset * 1e3).toISOString().slice(-11, -8)
+        this.icon = data.weather[0].icon
+    }
 
-  weatherTemplate() {
-    return `
+    weatherTemplate() {
+        return `
       <div class="text-white" id="weather-div">
 				  <h5 class="mt-1">${this.city} Weather &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;<img id=wx-icon" src="https://openweathermap.org/img/w/${this.icon}.png"> </h5>
 				  <p>Sky condition: ${this.description}</p>
@@ -23,6 +23,6 @@ export default class Weather {
           <p>Sunset: ${this.sunset}</p>
 		  </div>
           `
-  }
+    }
 
 }
