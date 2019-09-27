e2ab91f02a69ef4c17cb1563316f118f5bb47a9c
Brett Wilcox <wilcox.brett@gmail.com>
2019-09-26 21:41:22 -0600

    Last update tonight.

--- app/models/price.js
+++ app/models/price.js
@@ -1,12 +1,12 @@
 export default class Price {
     constructor(data) {
-        this.rate = data
+        this.rate = '$' + data
     }
 
     priceTemplate() {
         return `
       <div class="" id="price-div">
-          <h6 class="text-white font-italic mt-1">${this.rate}</h6> 
+          <h6 class="text-white font-italic mt-1">BTC ${this.rate}</h6> 
       </div>
           `
     }
