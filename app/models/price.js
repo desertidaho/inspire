f3c0927c7c2a37bcc1e619502cf5c39885ed36ec
Brett Wilcox <wilcox.brett@gmail.com>
2019-10-08 10:32:48 -0600

    Crypto prices, format.

--- app/models/price.js
+++ app/models/price.js
@@ -1,42 +1,89 @@
 export default class Price {
     constructor(data) {
-        this.btc = data[0].id;
-        this.btcName = this.btc.charAt(0).toUpperCase() + this.btc.slice(1)
-        this.btcPrice = '$' + Number(data[0].priceUsd).toFixed(2);
-        this.btc24HourChange = Number(data[0].changePercent24Hr).toFixed(2);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id == 'bitcoin') {
+                this.btcRank = data[i].rank
+                this.btcName = data[i].name
+                this.btcPrice = '$' + Number(data[i].priceUsd).toFixed(2)
+                this.btc24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+            if (data[i].id == 'ethereum') {
+                this.ethRank = data[i].rank
+                this.ethName = data[i].name
+                this.ethPrice = '$' + Number(data[i].priceUsd).toFixed(2);
+                this.eth24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+            if (data[i].id == 'ripple') {
+                this.xrpRank = data[i].rank
+                this.xrpName = data[i].name
+                this.xrpPrice = '$' + Number(data[i].priceUsd).toFixed(2);
+                this.xrp24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+            if (data[i].id == 'litecoin') {
+                this.ltcRank = data[i].rank
+                this.ltcName = data[i].name
+                this.ltcPrice = '$' + Number(data[i].priceUsd).toFixed(2);
+                this.ltc24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+            if (data[i].id == 'stellar') {
+                this.xlmRank = data[i].rank
+                this.xlmName = data[i].name
+                this.xlmPrice = '$' + Number(data[i].priceUsd).toFixed(2);
+                this.xlm24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+            if (data[i].id == 'chainlink') {
+                this.linkRank = data[i].rank
+                this.linkName = data[i].name
+                this.linkPrice = '$' + Number(data[i].priceUsd).toFixed(2);
+                this.link24HourChange = Number(data[i].changePercent24Hr).toFixed(2)
+            }
+        }
 
-        this.eth = data[1].id;
-        this.ethName = this.eth.charAt(0).toUpperCase() + this.eth.slice(1)
-        this.ethPrice = '$' + Number(data[1].priceUsd).toFixed(2);
-        this.eth24HourChange = Number(data[1].changePercent24Hr).toFixed(2);
-
-        this.ltc = data[5].id;
-        this.ltcName = this.ltc.charAt(0).toUpperCase() + this.ltc.slice(1)
-        this.ltcPrice = '$' + Number(data[5].priceUsd).toFixed(2);
-        this.ltc24HourChange = Number(data[5].changePercent24Hr).toFixed(2);
     }
 
     priceTemplate() {
         return `
         <tr class="table-header">
-		    <th width="50%"></th>
-			<th width="40%">Price</th>
-			<th id="change24" width="10%">Change/24hr</th>
+            <th width="15%">#</th>
+		    <th width="37%">Crypto</th>
+			<th width="38%">Price</th>
+			<th id="change24" width="10%">&Delta;/24hr</th>
         </tr>
         <tr class="table-rows">
+            <td>${this.btcRank}</td>
             <td>${this.btcName}</td>
             <td>${this.btcPrice}</td>
-            <td align="center">${this.btc24HourChange}%</td>
+            <td align="right">${this.btc24HourChange}%</td>
         </tr>
         <tr class="table-rows">
+            <td>${this.ethRank}</td>
             <td>${this.ethName}</td>
             <td>${this.ethPrice}</td>
-            <td align="center">${this.eth24HourChange}%</td>
+            <td align="right">${this.eth24HourChange}%</td>
         </tr>
         <tr class="table-rows">
+            <td>${this.xrpRank}</td>
+            <td>${this.xrpName}</td>
+            <td>${this.xrpPrice}</td>
+            <td align="right">${this.xrp24HourChange}%</td>
+        </tr>
+        <tr class="table-rows">
+            <td>${this.ltcRank}</td>
             <td>${this.ltcName}</td>
             <td>${this.ltcPrice}</td>
-            <td align="center">${this.ltc24HourChange}%</td>
+            <td align="right">${this.ltc24HourChange}%</td>
+        </tr>
+        <tr class="table-rows">
+            <td>${this.xlmRank}</td>
+            <td>${this.xlmName}</td>
+            <td>${this.xlmPrice}</td>
+            <td align="right">${this.xlm24HourChange}%</td>
+        </tr>
+        <tr class="table-rows">
+            <td>${this.linkRank}</td>
+            <td>${this.linkName}</td>
+            <td>${this.linkPrice}</td>
+            <td align="right">${this.link24HourChange}%</td>
         </tr>
           `
     }
