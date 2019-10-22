b7cd13d4741370e4e198034b5147297e9358599f
Brett Wilcox <wilcox.brett@gmail.com>
2019-10-22 11:23:24 -0600

    Refactor news.

--- app/components/news/newsController.js
+++ app/components/news/newsController.js
@@ -4,20 +4,9 @@ export default class NewsController {
 
   openNews() {
     window.open('https://twitter.com/home');
-    this.openNews1()
-  }
-
-  openNews1() {
     window.open('https://www.rt.com/news/');
-    this.openNews2()
-  }
-
-  openNews2() {
     window.open('https://www.drudgereport.com/');
-    this.openNews3()
-  }
-
-  openNews3() {
     window.open('https://www.zerohedge.com/');
   }
+
 }
