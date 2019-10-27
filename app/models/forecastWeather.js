65dff4c338b3ae9211e83a00d677df2104c16a64
Brett Wilcox <wilcox.brett@gmail.com>
2019-10-27 17:43:59 -0600

    Forecast.

--- app/models/forecastWeather.js
+++ app/models/forecastWeather.js
@@ -1,13 +1,13 @@
 export default class ForecastWeather {
   constructor(data) {
-    this.description = data[7].weather[0].description
-    this.kelvin = data[7].main.temp
+    this.description = data[8].weather[0].description
+    this.kelvin = data[8].main.temp
     this.celcius = (this.kelvin - 273.15).toFixed(0)
     // @ts-ignore
     this.farenheit = (this.celcius * (9 / 5) + 32).toFixed(0)
-    this.windDegrees = (data[7].wind.deg).toFixed(0)
-    this.windSpeed = (data[7].wind.speed).toFixed(0)
-    this.icon = data[7].weather[0].icon
+    this.windDegrees = (data[8].wind.deg).toFixed(0)
+    this.windSpeed = (data[8].wind.speed).toFixed(0)
+    this.icon = data[8].weather[0].icon
 
     if (this.windDegrees < 10) {
       this.windDegrees = '00' + this.windDegrees
