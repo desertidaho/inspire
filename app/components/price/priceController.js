acbfc3f6fa7481c2e9edd2c7c0ab2e2941f5d7d9
Brett Wilcox <wilcox.brett@gmail.com>
2019-12-07 15:24:56 -0700

    Flash green and say update prices every 20 seconds

--- app/components/price/priceController.js
+++ app/components/price/priceController.js
@@ -3,8 +3,16 @@ import PriceService from "./priceService.js";
 const _ps = new PriceService()
 
 function drawPrice() {
+    document.getElementById('refresh-prices').style.visibility = "visible"
+    document.getElementById('price-div').style.background = "rgb(4, 228, 4)"
     let template = _ps.Price.priceTemplate()
     document.querySelector('#price').innerHTML = template
+    setTimeout(function () {
+        document.getElementById('refresh-prices').style.visibility = "hidden"
+    }, 1000);
+    setTimeout(function () {
+        document.getElementById('price-div').style.background = "black"
+    }, 1000);
 }
 
 function drawMetalPrice() {
